dfb9d27795d4c8254dcca4ea875066c185e8e4b6
marc <wangchengda1990@gmail.com>
2017-08-10 14:02:33 +0800

    贝塞尔圆

--- app/src/main/java/marc/com/viewadavanced/MainActivity.java
+++ app/src/main/java/marc/com/viewadavanced/MainActivity.java
@@ -15,11 +15,11 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		SpiderView spiderview = (SpiderView) findViewById(R.id.spider_view);
+		/*SpiderView spiderview = (SpiderView) findViewById(R.id.spider_view);
 		List<SpiderData> datas = new ArrayList<>();
 		for (int i=0;i<6;i++){
 			datas.add(new SpiderData(""+i,i));
 		}
-		spiderview.setData(datas);
+		spiderview.setData(datas);*/
 	}
 }
